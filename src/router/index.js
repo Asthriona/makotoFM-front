6db3579102bd77b38cc19c789d7ae54534168c69
Makoto Kobayashi <im@asthriona.com>
2023-10-26 05:53:09 +0200

    feat(Router): :truck: Add /about route to router

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,17 @@ const routes = [
       },
     ],
   },
+  {
+    path: "/about",
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: "About",
+        component: () => import('@/views/about.vue')
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
