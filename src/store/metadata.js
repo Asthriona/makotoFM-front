95fdf5081708736505adb6e0616b59cfddada5cf
Makoto Kobayashi <im@asthriona.com>
2023-10-27 09:21:19 +0200

    feat(Store): :lipstick: Add next playing track to store

--- src/store/metadata.js
+++ src/store/metadata.js
@@ -7,14 +7,37 @@ export const useMetaStore = defineStore('metadata', {
         album: "",
         art: "https://cdn.asthriona.com/i/2022/08/_pn_220815_0628AM07114.png",
         isLive: false,
+        next: {
+            title: "",
+            artist: "",
+            album: "",
+            art: "",
+            isLive: false,
+            isRequest: false,
+            played_ago: "",
+            playedAt: "",
+            id: "",
+        }
     }),
     actions: {
         setMetadata(data) {
             this.title = data.title,
-            this.artist = data.artist,
-            this.album = data.album,
-            this.art = data.art ? data.art : "https://cdn.asthriona.com/i/2022/08/_pn_220815_0628AM07114.png",
-            this.isLive = data.isLive
+                this.artist = data.artist,
+                this.album = data.album,
+                this.art = data.art ? data.art : "https://cdn.asthriona.com/i/2022/08/_pn_220815_0628AM07114.png",
+                this.isLive = data.isLive,
+                this.next = {
+                    title: data.next.title,
+                    artist: data.next.artist,
+                    album: data.next.album,
+                    art: data.next.art,
+                    isLive: data.next.isLive,
+                    isRequest: data.next.isRequest,
+                    played_ago: data.next.played_ago,
+                    playedAt: data.next.playedAt,
+                    id: data.next.id,
+
+                }
         }
     }
 })
