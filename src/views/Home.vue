b58d8963cf0ff2d1eb28b1e7ad2301618f79efb6
Asthriona <im@asthriona.com>
2025-04-18 14:26:15 +0200

    feat: add Footer component to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
   <HelloWorld />
+  <Footer />
 </template>
 
 <script>
+import HelloWorld from "@/components/HelloWorld.vue";
+import Footer from "@/components/TheFooter.vue"
 export default {
   name: "home-view",
+  components: {
+    HelloWorld,
+    Footer,
+  },
 };
 </script>
